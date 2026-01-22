8aec57ea8c51a3de776f183cd8cc81094ffbf266
Moumen Hamid <moumen.hamid@outlook.com>
2026-01-22 17:13:37 +0300

    added test files

---
+++ example/convex/setup.test.ts
@@ -0,0 +1,16 @@
+/// <reference types="vite/client" />
+import { convexTest } from "convex-test";
+import component from "convex-tracer/test";
+import { test } from "vitest";
+import schema from "./schema.js";
+
+const modules = import.meta.glob("./**/*.*s");
+// When users want to write tests that use your component, they need to
+// explicitly register it with its schema and modules.
+export function initConvexTest() {
+  const t = convexTest(schema, modules);
+  component.register(t);
+  return t;
+}
+
+test("setup", () => {});
